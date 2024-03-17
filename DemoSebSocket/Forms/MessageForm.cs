f690434607ee0be693f9d420cd951bbccaab6d4e
Khun <khun.ly@bstorm.be>
2024-03-17 11:49:05 +0100

    Add project files.

---
+++ DemoSebSocket/Forms/MessageForm.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoSebSocket.Forms
+{
+    public class MessageForm
+    {
+        public string Author { get; set; } = null!;
+        public string Content { get; set; } = null!;
+    }
+}
---
+++ DemoSebSocket/Hubs/MessageHub.cs
@@ -0,0 +1,18 @@
+﻿using DemoSebSocket.Forms;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DemoSebSocket.Hubs
+{
+    public class MessageHub: Hub
+    {
+        public async Task SendMessage(MessageForm form)
+        {
+            await Clients.Others.SendAsync("newMessage", new
+            {
+                Author = form.Author,
+                Content = form.Content,
+                Date = DateTime.Now,
+            });
+        }
+    }
+}
